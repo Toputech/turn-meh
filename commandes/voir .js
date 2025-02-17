1dd3fa820e85a74c2627a4eb6a47233f77df6234
Toputech <168991681+Toputech@users.noreply.github.com>
2025-02-17 16:05:14 +0300

    Update voir .js
    const {zokou}=require("../framework/zokou")
    const {getContentType}=require("@whiskeysockets/baileys")
    
    
    
    zokou({nomCom:"vv",categorie:"system",reaction:"🫣"},async(dest,zk,commandeOptions)=>{
    
    const {ms,msgRepondu,repondre}=commandeOptions;
    
    
    if(!msgRepondu){return repondre("*Mention a view once media* .");}
    
    
    if(msgRepondu.viewOnceMessageV3)
    {
          if(msgRepondu.viewOnceMessageV3.message.imageMessage)
           {
             var image =await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV3.message.imageMessage)
            var texte = msgRepondu.viewOnceMessageV3.message.imageMessage.caption
        
         await zk.sendMessage(dest,{image:{url:image},caption:texte},{quoted:ms})
          }else if(msgRepondu.viewOnceMessageV3.message.videoMessage){
    
        var video = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV3.message.videoMessage)
    var texte =msgRepondu.viewOnceMessageV3.message.videoMessage.caption
    
    
    await zk.sendMessage(dest,{video:{url:video},caption:texte},{quoted:ms})
    
    }
    }else
    {
       return repondre("```this message is not on view once .```")
    }
    
    
    
    })

--- commandes/voir .js
+++ commandes/voir .js
@@ -3,26 +3,26 @@ const {getContentType}=require("@whiskeysockets/baileys")
 
 
 
-zokou({nomCom:"vv",categorie:"General",reaction:"🫣"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"vv",categorie:"system",reaction:"🫣"},async(dest,zk,commandeOptions)=>{
 
 const {ms,msgRepondu,repondre}=commandeOptions;
 
 
-if(!msgRepondu){return repondre("*Mentionne a view once media* .");}
+if(!msgRepondu){return repondre("*Mention a view once media* .");}
 
 
-if(msgRepondu.viewOnceMessageV2)
+if(msgRepondu.viewOnceMessageV3)
 {
-      if(msgRepondu.viewOnceMessageV2.message.imageMessage)
+      if(msgRepondu.viewOnceMessageV3.message.imageMessage)
        {
-         var image =await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.imageMessage)
-        var texte = msgRepondu.viewOnceMessageV2.message.imageMessage.caption
+         var image =await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV3.message.imageMessage)
+        var texte = msgRepondu.viewOnceMessageV3.message.imageMessage.caption
     
      await zk.sendMessage(dest,{image:{url:image},caption:texte},{quoted:ms})
-      }else if(msgRepondu.viewOnceMessageV2.message.videoMessage){
+      }else if(msgRepondu.viewOnceMessageV3.message.videoMessage){
 
-    var video = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV2.message.videoMessage)
-var texte =msgRepondu.viewOnceMessageV2.message.videoMessage.caption
+    var video = await zk.downloadAndSaveMediaMessage(msgRepondu.viewOnceMessageV3.message.videoMessage)
+var texte =msgRepondu.viewOnceMessageV3.message.videoMessage.caption
 
 
 await zk.sendMessage(dest,{video:{url:video},caption:texte},{quoted:ms})
@@ -30,7 +30,7 @@ await zk.sendMessage(dest,{video:{url:video},caption:texte},{quoted:ms})
 }
 }else
 {
-   return repondre("this message is not on view once .")
+   return repondre("```this message is not on view once .```")
 }
 
 
