7bd9c968fc7ec240ce698ac555e608fbb314e879
Toputech <168991681+Toputech@users.noreply.github.com>
2025-02-19 13:09:45 +0300

    Update bugs.js

--- commandes/bugs.js
+++ commandes/bugs.js
@@ -5,7 +5,7 @@ const util = require('util');
 const fs = require('fs-extra');
 const { format } = require(__dirname + "/../framework/mesfonctions");
 const os = require("os");
-const conf = require("../set.js");
+const conf = require(__dirname + "/../set");
 const path = require("path");
 const {
     generateWAMessageFromContent,
