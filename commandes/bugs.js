f37356f4c154230ebb38ebb8b653f5efe379aaba
Toputech <168991681+Toputech@users.noreply.github.com>
2025-02-21 20:39:47 +0300

    Update bugs.js

--- commandes/bugs.js
+++ commandes/bugs.js
@@ -1,5 +1,5 @@
-const { zokou } = require("../framework/zokou");
-const { delay, loading, react } = require("../framework/utils");
+
+const { delay, loading, react } = require(__dirname + "../framework/utils");
 const moment = require("moment-timezone");
 const conf = require(__dirname + "/../set");
 const fs = require('fs-extra');
