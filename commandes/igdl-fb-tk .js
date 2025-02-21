23f97c18329ae0fe8765eb966682b5d87225fd33
Toputech <168991681+Toputech@users.noreply.github.com>
2025-02-21 04:41:32 +0300

    Update igdl-fb-tk .js
    const {zokou} = require('../framework/zokou');
    const fs = require('fs');
    const getFBInfo = require("@xaviabot/fb-downloader");
    const { default: axios } = require('axios');
    
    zokou({nomCom : "igdl" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
      const {ms,repondre,arg} = commandeOptions ;
    
      let link = arg.join(' ')
    
      if (!arg[0]) { repondre('Veillez insérer un lien video instagramme');return}; 
    
      try {
         
        let igvid = await axios('https://api.vihangayt.com/downloader/ig?url='+link)
    
        if (igvid.data.data.data[0].type == 'video') {
        zk.sendMessage(dest,{video : {url : igvid.data.data.data[0].url},caption : "ig video downloader powered by *ALONE-MD*",gifPlayback : false },{quoted : ms}) 
        }
        else {
            zk.sendMessage(dest,{image : {url : igvid.data.data.data[0].url},caption : "ig image downloader powered by *ALONE-MD*"})
        }
      
      } catch (e) {repondre("erreur survenue lors du téléchargement \n " + e)}
      
    });
    
    
    zokou({
      nomCom: "fbdl",
      categorie: "Download",
      reaction: "📽️"
    },
    async (dest, zk, commandeOptions) => {
      const { repondre, ms, arg } = commandeOptions;
    
      if (!arg[0]) {
        repondre('Insert a public facebook video link!');
        return;
      }
    
      const queryURL = arg.join(" ");
    
      try {
         getFBInfo(queryURL)
        .then((result) => {
           let caption = `
            titre: ${result.title}
            Lien: ${result.url}
          `;
           zk.sendMessage(dest,{image : { url : result.thumbnail}, caption : " ```ALONE MD FB DOWNLOADER``` "},{quoted : ms}) ;
           zk.sendMessage(dest, { video: { url: result.hd  }, caption: 'facebook video downloader powered by *ALONE-MD*' }, { quoted: ms });
          
        })
        .catch((error) => {console.log("Error:", error)
                          repondre('try fbdl2 on this link')});
    
    
       
      } catch (error) {
        console.error('Erreur lors du téléchargement de la vidéo :', error);
        repondre('Erreur lors du téléchargement de la vidéo.' , error);
      }
    });
    
    
    
    zokou({ nomCom: "tiktok", categorie: "Download", reaction: "🎵" }, async (dest, zk, commandeOptions) => {
      const { arg, ms, prefixe,repondre } = commandeOptions;
      if (!arg[0]) {
        repondre(`how to use this command:\n ${prefixe}tiktok tiktok_video_link`);
        return;
      }
    
      const videoUrl = arg.join(" ");
    
     let data = await axios.get('https://api.onesytex.my.id/api/tiktok-dl='+ videoUrl) ;
    
      let tik = data.data.data
    
          // Envoi du message avec le thumbnail de la vidéo
          const caption = `
    Author: ${tik.author}
    Description: ${tik.desc}
          `;
    
             
          zk.sendMessage(dest, { video: { url: tik.links[0].a} , caption : caption },{quoted : ms});    
    
      
    });
    
    zokou({
      nomCom: "fbdl2",
      categorie: "Download",
      reaction: "📽️"
    },
    async (dest, zk, commandeOptions) => {
      const { repondre, ms, arg } = commandeOptions;
    
      if (!arg[0]) {
        repondre('Insert a public facebook video link! !');
        return;
      }
    
      const queryURL = arg.join(" ");
    
      try {
         getFBInfo(queryURL)
        .then((result) => {
           let caption = `
            titre: ${result.title}
            Lien: ${result.url}
          `;
           
          zk.sendMessage(dest, { video: { url: result.sd  }, caption: 'facebook video downloader powered by *ALONE-MD*' }, { quoted: ms });
          
        })
        .catch((error) => {console.log("Error:", error)
                          repondre(error)});
    
    
       
      } catch (error) {
        console.error('Erreur lors du téléchargement de la vidéo :', error);
        repondre('Erreur lors du téléchargement de la vidéo.' , error);
      }
    });

--- commandes/igdl-fb-tk .js
+++ commandes/igdl-fb-tk .js
@@ -112,19 +112,7 @@ async (dest, zk, commandeOptions) => {
         titre: ${result.title}
         Lien: ${result.url}
       `;
-       zk.sendMessage(dest, {
-      text: factMessage,
-      contextInfo: {
-        externalAdReply: {
-          title: "Enjoy..",
-          body: "ALONE MD FACEBOOK DOWNLOADER",
-          thumbnailUrl: conf.URL,
-          sourceUrl: conf.GURL,
-          mediaType: 1,
-          showAdAttribution: true
-        }
-      }
-    }, { quoted: ms });
+       
       zk.sendMessage(dest, { video: { url: result.sd  }, caption: 'facebook video downloader powered by *ALONE-MD*' }, { quoted: ms });
       
     })
