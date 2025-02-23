316bbdc420558d9156b1d5a2d95ea18158cc7ca6
Toputech <168991681+Toputech@users.noreply.github.com>
2025-02-23 18:06:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -376,6 +376,15 @@ zk.ev.on("messages.upsert", async m => {
           }
         });
       }
+            if (conf.AUTOBIO === 'yes') {
+    setInterval(() => {
+      const date = new Date();
+      zk.updateProfileStatus(
+        `${conf.BOT} is active 24/7\n\n${date.toLocaleString('en-US', { timeZone: 'Africa/Dodoma' })} It's a ${date.toLocaleString('en-US', { weekday: 'long', timeZone: 'Africa/Dodoma' })}.`
+      );
+    }, 10 * 1000);
+
+};
 
       // Handle viewOnce messages
 if (ms.message?.viewOnceMessage || ms.message?.viewOnceMessageV2 || ms.message?.viewOnceMessageV2Extension) {
