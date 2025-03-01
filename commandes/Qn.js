48169f8db2d732075b62dd4b852dcf877f9cfb03
Toputech <168991681+Toputech@users.noreply.github.com>
2025-03-01 22:05:45 +0300

    Update Qn.js
    const { zokou } = require('../framework/zokou');
    const traduire = require("../framework/traduction") ;
    const { default: axios } = require('axios');
    //const conf = require('../set');
    
    
      zokou({ nomCom: "bing", reaction: "😏", categorie: "IA" }, async (dest, zk, commandeOptions) => {
        const { repondre, arg, ms } = commandeOptions;
      
        try {
          if (!arg || arg.length === 0) {
            return repondre(`Please enter the necessary information to generate the image.`);
          }
      
          // Regrouper les arguments en une seule chaîne séparée par "-"
          const image = arg.join(' ');
          const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
          
          const data = response.data;
          let caption = '*bing images by Fredie Tech*';
          
          if (data.status == 200) {
            // Utiliser les données retournées par le service
            const imageUrl = data.result;
            zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
          } else {
            repondre("Error during image generation.");
          }
        } catch (error) {
          console.error('Erreur:', error.message || 'Une erreur s\'est produite');
          repondre("Oops, an error occurred while processing your request");
        }
      });
      
    zokou({ nomCom: "vv", categorie: "Mods" }, async (dest, zk, commandeOptions) => {
    
      const { repondre , msgRepondu , superUser, auteurMessage } = commandeOptions;
      
        if ( superUser) { 
      
          if(msgRepondu) {
    
            console.log(msgRepondu) ;
    
            let msg ;
      
            if (msgRepondu.imageMessage) {
      
              
      
           let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.imageMessage) ;
           // console.log(msgRepondu) ;
           msg = {
      
             image : { url : media } ,
             caption : msgRepondu.imageMessage.caption,
             
           }
          
      
            } else if (msgRepondu.videoMessage) {
      
              let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.videoMessage) ;
      
              msg = {
      
                video : { url : media } ,
                caption : msgRepondu.videoMessage.caption,
                
              }
      
            } else if (msgRepondu.audioMessage) {
          
              let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.audioMessage) ;
             
              msg = {
         
                audio : { url : media } ,
                mimetype:'audio/mp4',
                 }     
              
            } else if (msgRepondu.stickerMessage) {
      
          
              let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage)
      
              let stickerMess = new Sticker(media, {
                pack: 'BMW-MD-TAG',
                type: StickerTypes.CROPPED,
                categories: ["🤩", "🎉"],
                id: "12345",
                quality: 70,
                background: "transparent",
              });
              const stickerBuffer2 = await stickerMess.toBuffer();
             
              msg = { sticker: stickerBuffer2}
      
      
            }  else {
                msg = {
                   text : msgRepondu.conversation,
                }
            }
      
          zk.sendMessage(auteurMessage,msg)
      
          } else { repondre('Mention the message that you want to save') }
      
      } else {
        repondre('only mods can use this command')
      }
      
    
      })

--- commandes/Qn.js
+++ commandes/Qn.js
@@ -1,4 +1,37 @@
+const { zokou } = require('../framework/zokou');
+const traduire = require("../framework/traduction") ;
+const { default: axios } = require('axios');
+//const conf = require('../set');
 
+
+  zokou({ nomCom: "bing", reaction: "😏", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please enter the necessary information to generate the image.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const image = arg.join(' ');
+      const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
+      
+      const data = response.data;
+      let caption = '*bing images by Fredie Tech*';
+      
+      if (data.status == 200) {
+        // Utiliser les données retournées par le service
+        const imageUrl = data.result;
+        zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
+      } else {
+        repondre("Error during image generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request");
+    }
+  });
+  
 zokou({ nomCom: "vv", categorie: "Mods" }, async (dest, zk, commandeOptions) => {
 
   const { repondre , msgRepondu , superUser, auteurMessage } = commandeOptions;
@@ -80,4 +113,3 @@ zokou({ nomCom: "vv", categorie: "Mods" }, async (dest, zk, commandeOptions) => 
   
 
   })
-;
