558486a536982c9a32c42fdbf3f322a3de68d67c
Toputech <168991681+Toputech@users.noreply.github.com>
2025-03-08 13:21:43 +0300

    Update menu1.js

--- commandes/menu1.js
+++ commandes/menu1.js
@@ -28,7 +28,7 @@ zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
         coms[com.categorie].push(com.nomCom);
     });
 
-    moment.tz.setDefault("Africa/Nairobi");
+    moment.tz.setDefault(${s.TZ}");
 
 // Créer une date et une heure en GMT
 const temps = moment().format('HH:mm:ss');
