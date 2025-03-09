ca043e318f18e2d27f58845b78bc2d21f15055f4
Toputech <168991681+Toputech@users.noreply.github.com>
2025-03-09 13:36:27 +0300

    Update voir .js

--- commandes/voir .js
+++ commandes/voir .js
@@ -29,7 +29,7 @@ zokou({ nomCom: "pi", categorie: "General" }, async (dest, zk, commandeOptions) 
             text:`❣️ *Pong:* ${ping}ms\n📅 *Date:* ${date}\n⏰ *Time:* ${time}`, 
             ptt: true, // Voice note form
             contextInfo: {
-      mentionedJid: m.sender,
+      mentionedJid: "https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r",
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
