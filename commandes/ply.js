248e4a15ffd8eb6c9ba0375a0644ab23cf98e2fa
Toputech <168991681+Toputech@users.noreply.github.com>
2025-03-13 00:33:03 +0300

    Update ply.js

--- commandes/ply.js
+++ commandes/ply.js
@@ -8,7 +8,7 @@ zokou({
   nomCom: "play",
   aliases: ["song", "playdoc", "audio", "mp3"],
   categorie: "Search",
-  reaction: "❤️"
+  reaction: ""
 }, async (dest, zk, commandOptions) => {
   const { arg, ms, repondre } = commandOptions;
 
@@ -64,21 +64,6 @@ zokou({
 
     const downloadUrl = downloadData.result.download_url;
     const videoDetails = downloadData.result;
-    zk.sendMessage(dest, {
-      text: "```ALONE MD SONG DOWNLOADER```",
-      audio: { url: downloadUrl },
-        mimetype: 'audio/mp4',
-      contextInfo: {
-        externalAdReply: {
-          title: "Follow for fantastic updates",
-          body: "ALONE MD SONG DOWNLOADER",
-          thumbnailUrl: conf.URL,
-          sourceUrl: conf.GURL,
-          mediaType: 1,
-          showAdAttribution: true
-        }
-      }
-    }, { quoted: ms });
 
     // Prepare the message payload with external ad details
     const messagePayloads = [
@@ -96,100 +81,24 @@ zokou({
             showAdAttribution: true,
           },
         },
-      }
-
-    ];
-
-    // Send the download link to the user for each payload
-    for (const messagePayload of messagePayloads) {
-      await zk.sendMessage(dest, messagePayload, { quoted: ms });
-    }
-
-  } catch (error) {
-    console.error('Error during download process:', error);
-    return repondre(`Download failed due to an error: ${error.message || error}`);
-  }
-});
-zokou({
-  nomCom: "song",
-  aliases: ["song", "playdoc", "audio", "mp3"],
-  categorie: "Search",
-  reaction: "❤️"
-}, async (dest, zk, commandOptions) => {
-  const { arg, ms, repondre } = commandOptions;
-
-  // Check if a query is provided
-  if (!arg[0]) {
-    return repondre("Please provide a video name.");
-  }
-
-  const query = arg.join(" ");
-
-  try {
-    // Perform a YouTube search based on the query
-    const searchResults = await ytSearch(query);
-
-    // Check if any videos were found
-    if (!searchResults || !searchResults.videos.length) {
-      return repondre('No video found for the specified query.');
-    }
-
-    const firstVideo = searchResults.videos[0];
-    const videoUrl = firstVideo.url;
-
-    // Function to get download data from APIs
-    const getDownloadData = async (url) => {
-      try {
-        const response = await axios.get(url);
-        return response.data;
-      } catch (error) {
-        console.error('Error fetching data from API:', error);
-        return { success: false };
-      }
-    };
-
-    // List of APIs to try
-    const apis = [
-      `https://api-rin-tohsaka.vercel.app/download/ytmp4?url=${encodeURIComponent(videoUrl)}`,
-      `https://api.davidcyriltech.my.id/download/ytmp3?url=${encodeURIComponent(videoUrl)}`,
-      `https://www.dark-yasiya-api.site/download/ytmp3?url=${encodeURIComponent(videoUrl)}`,
-      `https://api.giftedtech.web.id/api/download/dlmp3?url=${encodeURIComponent(videoUrl)}&apikey=gifted-md`,
-      `https://api.dreaded.site/api/ytdl/audio?url=${encodeURIComponent(videoUrl)}`
-    ];
-
-    let downloadData;
-    for (const api of apis) {
-      downloadData = await getDownloadData(api);
-      if (downloadData && downloadData.success) break;
-    }
-
-    // Check if a valid download URL was found
-    if (!downloadData || !downloadData.success) {
-      return repondre('Failed to retrieve download URL from all sources. Please try again later.');
-    }
-
-    const downloadUrl = downloadData.result.download_url;
-    const videoDetails = downloadData.result;
-    zk.sendMessage(dest, {
-      text: "```ALONE MD SONG DOWNLOADER```",
-      audio: { url: downloadUrl },
-        mimetype: 'audio/mp4',
-      contextInfo: {
-        externalAdReply: {
-          title: "Follow for fantastic updates",
-          body: "ALONE MD SONG DOWNLOADER",
-          thumbnailUrl: conf.URL,
-          sourceUrl: conf.GURL,
-          mediaType: 1,
-          showAdAttribution: true
-        }
-      }
-    }, { quoted: ms });
-
-    // Prepare the message payload with external ad details
-    const messagePayloads = [
+      },
       {
-        audio: { url: downloadUrl },
+        document: { url: downloadUrl },
+        mimetype: 'audio/mpeg',
+        contextInfo: {
+          externalAdReply: {
+            title: videoDetails.title,
+            body: videoDetails.title,
+            mediaType: 1,
+            sourceUrl: conf.GURL,
+            thumbnailUrl: firstVideo.thumbnail,
+            renderLargerThumbnail: false,
+            showAdAttribution: true,
+          },
+        },
+      },
+      {
+        document: { url: downloadUrl },
         mimetype: 'audio/mp4',
         contextInfo: {
           externalAdReply: {
@@ -203,7 +112,6 @@ zokou({
           },
         },
       }
-
     ];
 
     // Send the download link to the user for each payload
@@ -278,27 +186,26 @@ zokou({
 
     const downloadUrl = downloadData.result.download_url;
     const videoDetails = downloadData.result;
-    zk.sendMessage(dest, {
-      text: "```ALONE MD VIDEO DOWNLOADER```",
-      audio: { url: downloadUrl },
-        mimetype: 'audio/mp4',
-      contextInfo: {
-        externalAdReply: {
-          title: "Follow for fantastic updates",
-          body: "ALONE MD VIDEO DOWNLOADER",
-          thumbnailUrl: conf.URL,
-          sourceUrl: conf.GURL,
-          mediaType: 1,
-          showAdAttribution: true
-        }
-      }
-    }, { quoted: ms });
-
 
     // Prepare the message payload with external ad details
     const messagePayloads = [
       {
         video: { url: downloadUrl },
+        mimetype: 'video/mp4',
+        contextInfo: {
+          externalAdReply: {
+            title: videoDetails.title,
+            body: videoDetails.title,
+            mediaType: 1,
+            sourceUrl: conf.GURL,
+            thumbnailUrl: firstVideo.thumbnail,
+            renderLargerThumbnail: false,
+            showAdAttribution: true,
+          },
+        },
+      },
+      {
+        document: { url: downloadUrl },
         mimetype: 'video/mp4',
         contextInfo: {
           externalAdReply: {
