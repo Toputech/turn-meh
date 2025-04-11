011aaa2a8141a0b363c1c859260aae6e4ed88164
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-11 21:19:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1329,8 +1329,10 @@ if (conf.AUTO_LIKE_STATUS === "yes") {
             if (connection === "connecting") {
                 console.log("ℹ️ ALONE-MD connecting in your account...");
             }
-            else if (connection === 'open')
-     console.log("✅ ALONE-MD connected successfully☺️");
+
+            else if (connection === 'open') {
+
+                console.log("✅ ALONE-MD connected successfully☺️");
                 console.log("--");
                 await (0, baileys_1.delay)(200);
                 console.log("------");
@@ -1366,7 +1368,7 @@ if (conf.AUTO_LIKE_STATUS === "yes") {
                 console.log("Alone-MD successfully connected✅");
 
                 await activateCrons();
-
+                
                 if((conf.DP).toLowerCase() === 'no') {     
                 let cmsg = `ALONE-MD  RUNNING ...
     
