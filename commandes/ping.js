0d548cfd9f604092b5b7346fde1cb12cd94d636d
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-11 17:31:28 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,23 +1,67 @@
-const { zokou } = require("../framework/zokou");
+
+
+
+
+
+
+
+
+
+
+
+
+
+const util = require('util');
+const fs = require('fs-extra');
+const axios = require('axios');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const os = require("os");
 const moment = require("moment-timezone");
-const { default: axios } = require('axios');
-//const conf = require('../set');
+const set = require(__dirname + "/../set");
 
+const AUDIO_URL = "https://files.catbox.moe/z2roat.m4v"; // New audio URL
+const THUMBNAIL_URL = "https://files.catbox.moe/yedfbr.jpg"; // New image URL
 
-zokou({ nomCom: 'ping',
-    desc: 'To check ping',
-    Categorie: 'General',
-    reaction: '❣️', 
-    fromMe: 'true', 
+moment.tz.setDefault(`${set.TZ}`);
 
-       
-  },
-  async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-    const { start} = new Date().getTime()
-    return repondre('*•𝘼𝙇𝙊𝙉𝙀-𝙈𝘿• ❣️        respond speed is*\n ```' +1000000990+ '``` *m/s*') 
-    const { end } = new Date().getTime()
-    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
-  }
-)
+const getTimeAndDate = () => {
+    return {
+        time: moment().format('HH:mm:ss'),
+        date: moment().format('DD/MM/YYYY')
+    };
+};
 
+// Ping Command
+zokou({ nomCom: "ping", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms } = commandeOptions;
+    const { time, date } = getTimeAndDate();
+    const ping = Math.floor(Math.random() * 100) + 1; // Generate a random ping between 1ms - 100ms
+
+    try {
+        await zk.sendMessage(dest, { 
+            audio: { url: AUDIO_URL }, 
+            mimetype: 'audio/mp4', 
+            ptt: true, // Voice note form
+            contextInfo: {      contextInfo: {
+        forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: '120363295141350550@newsletter',
+              newsletterName: 'ALONE Queen MD V²',
+              serverMessageId: 143},
+
+                externalAdReply: {
+                    title: "Alone Md Is At Infinite Speed ",
+                    body: `🎈 𝚙𝚒𝚗𝚐 🎈: ${ping}ms\n📅 *Date:* ${date}\n⏰ *Time:* ${time}`,
+                    thumbnailUrl: THUMBNAIL_URL,
+                    mediaType: 1,
+                    renderSmallThumbnail: true // Small thumbnail rendering
+                }
+            }
+        }, { quoted: ms });
+
+    } catch (e) {
+        console.log("❌ Ping Command Error: " + e);
+        repondre("❌ Error: " + e);
+    }
+});
