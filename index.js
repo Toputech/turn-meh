baff7d0402df3683e1cd27db4658e85a63b792af
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-14 11:15:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1329,8 +1329,7 @@ if (conf.AUTO_LIKE_STATUS === "yes") {
             if (connection === "connecting") {
                 console.log("ℹ️ ALONE-MD connecting in your account...");
             }
-             if (connection === 'open') {
-
+            else if (connection === 'open'){
                 console.log("✅ ALONE-MD connected successfully☺️");
                 console.log("--");
                 await (0, baileys_1.delay)(200);
