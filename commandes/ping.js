77da1aba2742f50c8ee019615b4fe03769f1bfbd
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-18 11:10:02 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -38,27 +38,26 @@ zokou({ nomCom: "ping", categorie: "General" }, async (dest, zk, commandeOptions
     const ping = Math.floor(Math.random() * 100) + 1; // Generate a random ping between 1ms - 100ms
 
     try {
-        await zk.sendMessage(dest, { 
-            audio: { url: AUDIO_URL }, 
-            mimetype: 'audio/mp3', 
-            ptt: true, // Voice note form
-            contextInfo: {
+        zk.sendMessage(dest, {
+        contextInfo: {
+          text: "This member cannot be removed because he is an administrator of the group",
         forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
               newsletterJid: '120363295141350550@newsletter',
               newsletterName: 'ALONE Queen MD V²',
               serverMessageId: 143},
-
-                externalAdReply: {
-                    title: "Alone Md Is At Infinite Speed ",
-                    body: `🎈 pong 🎈: ${ping}ms\n📅 *Date:* ${date}\n⏰ *Time:* ${time}`,
-                    thumbnailUrl: conf.URL,
-                    mediaType: 1,
-                    renderSmallThumbnail: true // Small thumbnail rendering
-                }
-            }
-        }, { quoted: ms });
+        externalAdReply: {
+          
+          title: "Follow for updates",
+          body: "Enjoy..!",
+          thumbnailUrl: conf.URL,
+          sourceUrl: conf.GURL,
+          mediaType: 1,
+          
+        }
+      }
+    }, { quoted: ms });       
 
     } catch (e) {
         console.log("❌ Ping Command Error: " + e);
